619ecc7716a032fc6c8fa3a8a0df65e8afc32836
Larry Garfield <larry@platform.sh>
2019-08-14 12:29:18 -0500

    Update counter.

--- web/index.php
+++ web/index.php
@@ -9,4 +9,4 @@ $messages = new Messages();
 printf("<h1>%s</h1>\n", $messages->title());
 printf("<p>%s</p>\n", $messages->message());
 
-printf("<p>Count: %s</p>\n", 8);
+printf("<p>Count: %s</p>\n", 9);
